0070026fa6e905dbebce6c489b9176bcb51b9d6e
maimorag <morag.mai10@gmail.com>
2025-07-08 13:17:46 +0300

    wip\

--- meal_tracking.py
+++ meal_tracking.py
@@ -1,45 +1,45 @@
 from sqlite3 import IntegrityError, OperationalError
-from typing import Union
+from typing import Any, Dict, Union
 import logging
 import sys
 import argparse
-
+import json
+from pathlib import Path# do I need metavar?
+# actions
 
 class GoalsOption(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         # Do something custom with `values` here
         setattr(namespace, self.dest, values)
 
+
+def read_file(pwd: Path) -> Dict[str, Any]:
+    with open(pwd, 'r') as file:
+        return file.read()
+
+
 def parser():
-    pass
-
-
-def goals_option(pwd: str):
-    print("goals")
-
-def meals_option(meals_path: str):
-    print(f"meals {meals_path}")
-    
-
-
-def user_input():
-    pass
-        
-
-
-
-def main():
-    logging.info("starting..")
     parser = argparse.ArgumentParser(description="A program that calculates\
         the total nutrition consumed from meals and how much is left for\
         the day based on your nutrition goals.",
         usage= "meal_tracking.py [-h] [-v] -g GOALS_PATH -m MEALS_PATH"
     )
-    parser.add_argument("-g","--goals",metavar="GOALS_PATH", help="Path to a file containing nutrition goals (JSON).", action=GoalsOption)
-    parser.add_argument("-m","--meals",metavar="MEALS_PATH", help="Path to a file containing meals eaten today (JSON).")
-    parser.add_argument("-e","--create_excel", required=False, help="create an excel based on the goals and meals given" )
-    args = parser.parse_args()
-    print(args)
+    parser.add_argument("-g","--goals",metavar="GOALS_PATH", type=Path, help="Path to a file containing nutrition goals (JSON).", action=GoalsOption)
+    parser.add_argument("-ig","--interactive-goals", help="Path to a file containing meals eaten today (JSON).")
+    parser.add_argument("-m","--meals",metavar="MEALS_PATH", type=Path,help="Path to a file containing meals eaten today (JSON).")
+    parser.add_argument("-im","--interactive-meals", help="Path to a file containing meals eaten today (JSON).")
+    parser.add_argument("-e","--create_excel", required=False, type= bool, help="create an excel based on the goals and meals given" )
+    #interactive meals/ goals
+    #adding argument of the excel that include the goals and meals so par
+    
+    return parser.parse_args()
+    
+
+def main():
+    args = parser()
+    meals = read_file(args.meals)
+    goals = read_file(args.goals)
+    print(f"hello {meals=} | {goals=}")
     
     
 if __name__ == "__main__":
